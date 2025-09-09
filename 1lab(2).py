4d5e6ddd5c0b3ef97f0a06f47523df5242a3f9b4
lorgovdanil <lorgovdanil@gmail.com>
2025-09-09 10:51:49 +0500

    Add files via upload

--- 1lab(2).py
+++ 1lab(2).py
@@ -1,17 +1,21 @@
 import math
 
+def func(x):
+    return x * x - math.sin(x)
+
+
 a = 0
 b = math.pi / 2
-s = 10**(-7)
-eps = 10**(-6)
+s = 10 ** (-7)
+eps = 10 ** (-6)
 i = 0
 
 while abs((b - a)) > eps:
     i = i + 1
     x1 = (a + b - s) / 2
     x2 = (a + b + s) / 2
-    func1 = x1*x1 - math.sin(x1)
-    func2 = x2*x2 - math.sin(x2)
+    func1 = func(x1)
+    func2 = func(x2)
     if func1 > func2:
         a = x1
     else:
--- 1lab(g).py
+++ 1lab(g).py
@@ -1,15 +1,19 @@
 import math
 
+def func(x):
+    return x * x - math.sin(x)
+
+
 a = 0
 b = math.pi / 2
-s = 10**(-7)
-eps = 10**(-6)
+s = 10 ** (-7)
+eps = 10 ** (-6)
 i = 0
 
-x2 = b / ((1 + 5**(1/2)) / 2)
+x2 = b / ((1 + 5 ** (1 / 2)) / 2)
 x1 = b - x2
-func1 = x1*x1 - math.sin(x1)
-func2 = x2*x2 - math.sin(x2)
+func1 = func(x1)
+func2 = func(x2)
 while abs((b - a)) > eps:
     i = i + 1
     if func1 > func2:
@@ -17,13 +21,15 @@ while abs((b - a)) > eps:
         x1 = x2
         x2 = b - x1 + a
         func1 = func2
-        func2 = x2*x2 - math.sin(x2)
+        func2 = func(x2)
     else:
         b = x2
         x2 = x1
         x1 = b - x2 + a
         func2 = func1
-        func1 = x1 * x1 - math.sin(x1)
-result = (a + b) / 2
-print("результат ", result)
+        func1 = func(x1)
+resultx = (a + b) / 2
+resulty = func(resultx)
+print("x", resultx)
+print("y", resulty)
 print("итерации ", i)
